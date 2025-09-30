0f3942e08baf130b6fd7246499f9af84ccd64222
Samuel Sarle <samuel@sarle.com>
2025-09-30 16:46:16 +0300

    Switch to `/v1/chat/completions` API

--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,10 @@ pub struct Options {
 pub fn generate(input: impl AsRef<str>, options: &Options) -> Result<Vec<String>, Box<dyn Error>> {
     let mut req = json!({
         "model": options.model,
-        "input": input.as_ref(),
+        "messages": [{
+            "role": "user",
+            "content": input.as_ref(),
+        }],
     });
 
     if let Some(max_tokens) = options.max_tokens {
@@ -41,7 +44,7 @@ pub fn generate(input: impl AsRef<str>, options: &Options) -> Result<Vec<String>
         .user_agent("asimov-openai-module")
         .build()
         .new_agent()
-        .post(format!("{}/v1/responses", options.endpoint))
+        .post(format!("{}/v1/chat/completions", options.endpoint))
         .header(
             "Authorization",
             format!("Bearer {}", options.api_key.expose_secret()),
@@ -77,106 +80,62 @@ pub fn generate(input: impl AsRef<str>, options: &Options) -> Result<Vec<String>
     }
 
     // {
-    //   "id": "resp_...",
-    //   "object": "response",
-    //   "created_at": 1758184601,
-    //   "status": "completed",
-    //   "background": false,
-    //   "billing": {
-    //     "payer": "developer"
-    //   },
-    //   "error": null,
-    //   "incomplete_details": null,
-    //   "instructions": null,
-    //   "max_output_tokens": null,
-    //   "max_tool_calls": null,
-    //   "model": "gpt-5-nano-2025-08-07",
-    //   "output": [
+    //   "id": "chatcmpl-...",
+    //   "object": "chat.completion",
+    //   "created": 1741569952,
+    //   "model": "gpt-4.1-2025-04-14",
+    //   "choices": [
     //     {
-    //       "id": "rs_...",
-    //       "type": "reasoning",
-    //       "summary": []
-    //     },
-    //     {
-    //       "id": "msg_...",
-    //       "type": "message",
-    //       "status": "completed",
-    //       "content": [
-    //         {
-    //           "type": "output_text",
-    //           "annotations": [],
-    //           "logprobs": [],
-    //           "text": "..."
-    //         }
-    //       ],
-    //       "role": "assistant"
+    //       "index": 0,
+    //       "message": {
+    //         "role": "assistant",
+    //         "content": "...",
+    //         "refusal": null,
+    //         "annotations": []
+    //       },
+    //       "logprobs": null,
+    //       "finish_reason": "stop"
     //     }
     //   ],
-    //   "parallel_tool_calls": true,
-    //   "previous_response_id": null,
-    //   "prompt_cache_key": null,
-    //   "reasoning": {
-    //     "effort": "medium",
-    //     "summary": null
-    //   },
-    //   "safety_identifier": null,
-    //   "service_tier": "default",
-    //   "store": true,
-    //   "temperature": 1.0,
-    //   "text": {
-    //     "format": {
-    //       "type": "text"
-    //     },
-    //     "verbosity": "medium"
-    //   },
-    //   "tool_choice": "auto",
-    //   "tools": [],
-    //   "top_logprobs": 0,
-    //   "top_p": 1.0,
-    //   "truncation": "disabled",
     //   "usage": {
-    //     "input_tokens": 7,
-    //     "input_tokens_details": {
-    //       "cached_tokens": 0
+    //     "prompt_tokens": 19,
+    //     "completion_tokens": 10,
+    //     "total_tokens": 29,
+    //     "prompt_tokens_details": {
+    //       "cached_tokens": 0,
+    //       "audio_tokens": 0
     //     },
-    //     "output_tokens": 261,
-    //     "output_tokens_details": {
-    //       "reasoning_tokens": 192
-    //     },
-    //     "total_tokens": 268
+    //     "completion_tokens_details": {
+    //       "reasoning_tokens": 0,
+    //       "audio_tokens": 0,
+    //       "accepted_prediction_tokens": 0,
+    //       "rejected_prediction_tokens": 0
+    //     }
     //   },
-    //   "user": null,
-    //   "metadata": {}
+    //   "service_tier": "default"
     // }
 
     let mut responses = Vec::new();
 
-    if let Some(chunks) = resp["output"].as_array() {
-        for chunk in chunks {
-            if chunk["type"].as_str().is_none_or(|t| t != "message") {
-                tracing::debug!("skipping non-message chunk in response: {chunk}");
+    if let Some(choices) = resp["choices"].as_array() {
+        // there is only one "choice" if the request doesn't have an "n" parameter
+        for choice in choices {
+            if choice["message"]["role"]
+                .as_str()
+                .is_none_or(|r| r != "assistant")
+            {
+                tracing::debug!("skipping output not from assistant: {choice}");
                 continue;
             }
-            if chunk["role"].as_str().is_none_or(|r| r != "assistant") {
-                tracing::debug!("skipping output chunk not from assistant: {chunk}");
-                continue;
+
+            if let Some(content) = choice["message"]["content"].as_str() {
+                responses.push(content.to_string())
+            } else if let Some(refusal) = choice["message"]["refusal"].as_str() {
+                tracing::error!("Request refused: {refusal}")
             }
 
-            if let Some(chunk_contents) = chunk["content"].as_array() {
-                for content in chunk_contents {
-                    if content["type"].as_str().is_none_or(|r| r != "output_text") {
-                        tracing::debug!("skipping non-text message chunk in response: {chunk}");
-                        continue;
-                    }
-
-                    if let Some(text) = content["text"].as_str() {
-                        responses.push(text.to_string());
-                    }
-                }
-            };
-
-            if let Some(status) = chunk["status"].as_str() {
-                tracing::debug!(status);
+            if let Some(finish_reason) = choice["finish_reason"].as_str() {
+                tracing::debug!(finish_reason);
             }
         }
     }
